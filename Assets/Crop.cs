2596860aaf63a6186b95b9033e86f4bc01ec0ba0
Sehwan <beullang172@gmail.com>
2025-06-15 19:35:20 +0900

    0615 7325

--- Assets/Crop.cs
+++ Assets/Crop.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 public class Crop : MonoBehaviour
 {
     private CropData data;
+    public Vector3Int cellPosition;
     private bool isWatered = false;
-    private bool isGrown = false;
-    private float growTimer = 0f;
-
-    public Sprite grownSprite;
+    private bool isMidGrown = false;
+    public bool isFullyWatered { get; private set; } = false;
+    public bool isFullyGrown { get; private set; } = false;
+    public FarmManager farmManager;
+    private float growTimer;
+    private float fullygrowTimer;
     private SpriteRenderer sr;
 
     void Start()
@@ -17,20 +20,32 @@ public class Crop : MonoBehaviour
 
     void Update()
     {
-        if (isWatered && !isGrown)
+        if (isWatered && !isMidGrown)
         {
             growTimer -= Time.deltaTime;
             if (growTimer <= 0f)
             {
-                Grow();
+                GrowToMid();
+            }
+        }
+        else if(isFullyWatered && !isFullyGrown)
+        {
+            fullygrowTimer -= Time.deltaTime;
+            if (fullygrowTimer <= 0f)
+            {
+                GrowToFull();
             }
         }
     }
 
-    public void SetData(CropData cropData)
+    public void SetData(CropData cropData, Vector3Int cellPos)
     {
         data = cropData;
         growTimer = data.growTime;
+        fullygrowTimer = data.growTime;
+        cellPosition = cellPos;
+        Debug.Log($"[SetData] 작물 데이터 설정됨. 성장 시간: {growTimer}");
+        
     }
 
     public void Water()
@@ -38,25 +53,37 @@ public class Crop : MonoBehaviour
         if (!isWatered)
         {
             isWatered = true;
-            Debug.Log("작물에 물을 주었습니다. 성장 시작!");
+            Debug.Log("물을 주고 작물 성장을 시작했습니다.");
+        }
+        else if (isMidGrown && !isFullyWatered)
+        {
+            isFullyWatered = true;
+            Debug.Log("다시 물을 주어 완전 성장을 시작합니다.");
+
         }
     }
 
-    void Grow()
+        void GrowToMid()
     {
-        isGrown = true;
-        sr.sprite = grownSprite;
-        Debug.Log("작물이 자랐습니다!");
+        isMidGrown = true;
+        sr.sprite = data.midGrowSprite;
+        Debug.Log("작물이 중간 단계로 성장했습니다.");
+        farmManager.ResetTileToTilled(cellPosition);
     }
 
-    public bool IsGrown()
+    void GrowToFull()
     {
-        return isGrown;
+        isFullyGrown = true;
+        sr.sprite = data.fullGrownSprite;
+        Debug.Log("작물이 완전히 자랐습니다.");
+        farmManager.ResetTileToTilled(cellPosition);
     }
+
+    public bool IsGrown() => isFullyGrown;
 
     public void Harvest()
     {
-        if (isGrown)
+        if (isFullyGrown)
         {
             Debug.Log($"[{data.cropName}] 수확 완료!");
             Destroy(gameObject);
